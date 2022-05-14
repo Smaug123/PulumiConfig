c5da015f16ff9b91cb5083f96d7de2e2f12020cf
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-05-14 15:51:46 +0100

    Move to actual prod config

---
+++ PulumiWebServer/Nginx.fs
@@ -0,0 +1,46 @@
+namespace PulumiWebServer
+
+type NginxConfig =
+    {
+        Domain : DomainName
+        WebSubdomain : string
+        AcmeEmail : EmailAddress
+    }
+
+    member this.Domains =
+        [ this.WebSubdomain ]
+        |> List.map (fun subdomain -> $"%s{subdomain}.{this.Domain}")
+        |> fun subdomains -> this.Domain.ToString () :: subdomains
+
+[<RequireQualifiedAccess>]
+module Nginx =
+
+    let private trimStart (s : string) (target : string) =
+        if target.StartsWith s then
+            target.[s.Length ..]
+        else
+            target
+
+    let createNixConfig (config : NginxConfig) : string =
+        let configTemplate =
+            Utils.getEmbeddedResource "nginx.nix"
+            |> fun s ->
+                s
+                    .Replace("@@DOMAIN@@", config.Domain.ToString ())
+                    .Replace("@@WEBROOT_SUBDOMAIN@@", config.WebSubdomain)
+                    .Replace ("@@ACME_EMAIL@@", config.AcmeEmail.ToString ())
+
+        let certConfig =
+            config.Domains
+            |> List.map (fun domain ->
+                [
+                    $"\"{domain}\" ="
+                    "{"
+                    "  server = \"https://acme-v02.api.letsencrypt.org/directory\";"
+                    "};"
+                ]
+                |> String.concat "\n"
+            )
+            |> String.concat "\n"
+
+        configTemplate.Replace ("\"@@DOMAINS@@\"", sprintf "{%s}" certConfig)
