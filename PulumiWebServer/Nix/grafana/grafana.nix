2954cce43259c421ab6142fcbe0edda0e0b5fc7e
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2025-12-27 17:24:18 +0000

    Centralise the Grafana logic (#54)

--- PulumiWebServer/Nix/grafana/grafana.nix
+++ PulumiWebServer/Nix/grafana/grafana.nix
@@ -3,33 +3,39 @@
   pkgs,
   lib,
   ...
-}: {
-  options = {
-    services.grafana-config = {
-      domain = lib.mkOption {
-        type = lib.types.str;
-        example = "example.com";
-        description = lib.mdDoc "Top-level domain to configure";
-      };
-      subdomain = lib.mkOption {
-        type = lib.types.str;
-        default = "grafana";
-        description = lib.mdDoc "Subdomain in which to put Grafana";
-      };
-      port = lib.mkOption {
-        type = lib.types.port;
-        description = lib.mdDoc "Grafana localhost port";
-        default = 2342;
-      };
+}: let
+  cfg = config.services.grafana-config;
+in {
+  options.services.grafana-config = {
+    enable = lib.mkEnableOption "Grafana monitoring dashboard";
+    domain = lib.mkOption {
+      type = lib.types.str;
+      example = "example.com";
+      description = lib.mdDoc "Top-level domain to configure";
+    };
+    subdomain = lib.mkOption {
+      type = lib.types.str;
+      default = "grafana";
+      description = lib.mdDoc "Subdomain in which to put Grafana";
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      description = lib.mdDoc "Grafana localhost port";
+      default = 2342;
     };
   };
 
-  config = {
-    services.nginx.virtualHosts."${config.services.grafana-config.subdomain}.${config.services.grafana-config.domain}" = {
+  config = lib.mkIf cfg.enable {
+    sops.secrets = {
+      "grafana_admin_password" = {owner = "grafana";};
+      "grafana_secret_key" = {owner = "grafana";};
+    };
+
+    services.nginx.virtualHosts."${cfg.subdomain}.${cfg.domain}" = {
       forceSSL = true;
       enableACME = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana-config.port}/";
+        proxyPass = "http://127.0.0.1:${toString cfg.port}/";
         proxyWebsockets = true;
       };
     };
@@ -45,10 +51,10 @@
       enable = true;
       settings = {
         server = {
-          domain = "${config.services.grafana-config.subdomain}.${config.services.grafana-config.domain}";
-          http_port = config.services.grafana-config.port;
+          domain = "${cfg.subdomain}.${cfg.domain}";
+          http_port = cfg.port;
           http_addr = "127.0.0.1";
-          root_url = "https://${config.services.grafana-config.subdomain}.${config.services.grafana-config.domain}";
+          root_url = "https://${cfg.subdomain}.${cfg.domain}";
         };
         security = {
           disable_initial_admin_creation = false;
@@ -68,7 +74,7 @@
           settings = {
             datasources = [
               {
-                name = "prometheus ${config.services.grafana-config.domain}";
+                name = "prometheus ${cfg.domain}";
                 type = "prometheus";
                 url = "http://127.0.0.1:${toString config.services.prometheus-config.port}";
                 access = "proxy";
