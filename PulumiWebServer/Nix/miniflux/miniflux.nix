f2b3e36608032fd9df7b71f3afc30979e7a76215
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-12-27 17:00:24 +0000

    Centralise the Miniflux logic

--- PulumiWebServer/Nix/miniflux/miniflux.nix
+++ PulumiWebServer/Nix/miniflux/miniflux.nix
@@ -3,38 +3,43 @@
   pkgs,
   lib,
   ...
-}: {
-  options = {
-    services.miniflux-config = {
-      domain = lib.mkOption {
-        type = lib.types.str;
-        example = "example.com";
-        description = lib.mdDoc "Top-level domain to configure";
-      };
-      subdomain = lib.mkOption {
-        type = lib.types.str;
-        example = "rss";
-        description = lib.mdDoc "Subdomain in which to put Miniflux";
-      };
-      port = lib.mkOption {
-        type = lib.types.port;
-        description = lib.mdDoc "Miniflux localhost port";
-        default = 8080;
-      };
+}: let
+  cfg = config.services.miniflux-config;
+in {
+  options.services.miniflux-config = {
+    enable = lib.mkEnableOption "Miniflux RSS reader";
+    domain = lib.mkOption {
+      type = lib.types.str;
+      example = "example.com";
+      description = lib.mdDoc "Top-level domain to configure";
+    };
+    subdomain = lib.mkOption {
+      type = lib.types.str;
+      example = "rss";
+      description = lib.mdDoc "Subdomain in which to put Miniflux";
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      description = lib.mdDoc "Miniflux localhost port";
+      default = 8080;
     };
   };
-  config = {
+
+  config = lib.mkIf cfg.enable {
+    sops.secrets = {
+      "miniflux_admin_password" = {owner = "miniflux";};
+    };
     users.users."miniflux".extraGroups = [config.users.groups.keys.name];
     services.miniflux = {
       enable = true;
       adminCredentialsFile = "/run/secrets/miniflux_admin_password";
     };
 
-    services.nginx.virtualHosts."${config.services.miniflux-config.subdomain}.${config.services.miniflux-config.domain}" = {
+    services.nginx.virtualHosts."${cfg.subdomain}.${cfg.domain}" = {
       forceSSL = true;
       enableACME = true;
       locations."/" = {
-        proxyPass = "http://localhost:${toString config.services.miniflux-config.port}/";
+        proxyPass = "http://localhost:${toString cfg.port}/";
       };
     };
   };
