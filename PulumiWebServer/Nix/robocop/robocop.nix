ad612ee7de8fd721b7724b003fe8b230194a5b44
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-12-27 16:01:20 +0000

    Add Robocop

---
+++ PulumiWebServer/Nix/robocop/robocop.nix
@@ -0,0 +1,69 @@
+{
+  config,
+  pkgs,
+  lib,
+  robocop,
+  ...
+}: {
+  options = {
+    services.robocop-config = {
+      domain = lib.mkOption {
+        type = lib.types.str;
+        example = "example.com";
+        description = lib.mdDoc "Top-level domain to configure";
+      };
+      subdomain = lib.mkOption {
+        type = lib.types.str;
+        example = "robocop";
+        description = lib.mdDoc "Subdomain in which to put the Robocop server";
+      };
+      port = lib.mkOption {
+        type = lib.types.port;
+        description = lib.mdDoc "Robocop localhost port to be forwarded";
+        default = 8259;
+      };
+    };
+  };
+
+  config = {
+    users.users."robocop".extraGroups = [config.users.groups.keys.name];
+    users.users."robocop".group = "robocop";
+    users.groups.robocop = {};
+    users.users."robocop".isSystemUser = true;
+
+    systemd.services.robocop-server = {
+      description = "robocop-server";
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        Restart = "always";
+        Type = "simple";
+        User = "robocop";
+        Group = "robocop";
+        ExecStart = "${robocop}/bin/robocop-server";
+        NoNewPrivileges = true;
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        PrivateTmp = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+
+        EnvironmentFile = "/etc/robocop/env";
+      };
+    };
+
+    services.nginx.virtualHosts."${config.services.robocop-config.subdomain}.${config.services.robocop-config.domain}" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://localhost:${toString config.services.robocop-config.port}/";
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-ForwardedProto $scheme;
+        '';
+      };
+    };
+  };
+}
